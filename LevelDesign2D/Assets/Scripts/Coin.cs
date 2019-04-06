e9fd2a4edaf50ef28a35a167d393f1a254821c0d
Luis Luna <lfluna@uci.edu>
2019-04-05 20:35:06 -0700

    Fixed stuff

---
+++ LevelDesign2D/Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour, Collectible
+{
+    public void DestroyOnTouch()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            DestroyOnTouch();
+    }
+
+}
